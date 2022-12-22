c2a442bdc97bd9093ebc027f6b2600390d51a6ad
Umair Shah <nicefellow1234@gmail.com>
2022-12-22 21:27:45 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+const { json } = require('body-parser');
+const { response } = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/assets/'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', function (req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = 'https://us21.api.mailchimp.com/3.0/lists/ece04075e0';
+
+  const options = {
+    method: 'POST',
+    auth: process.env.MAILCHIMP_API_KEY
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + '/success.html');
+    } else {
+      res.sendFile(__dirname + '/failure.html');
+    }
+
+    // respones.on("data", function (data) {
+    //     console.log(JSON.parse(data));
+    // });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post('/failure', function (req, res) {
+  res.redirect('/');
+});
+
+app.listen(3000, function () {
+  console.log('server is runnig on port 3000.');
+});
